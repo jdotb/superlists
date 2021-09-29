08e89e85f4382e010d56a5120f3c583238777957
jdotb <jdotbdotbold@gmail.com>
2021-09-29 14:08:32 -0400

    updated .gitignore, added project journal, added unittest to functional_test.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,23 +1,38 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-import django.core.urlresolvers
+# from django.http import HttpRequest
+# ^ REMOVED since using self.client.get()
 
-from lists.views import home_page
-
-
+"""
 class HomePageTest(TestCase):
 
     # First added test
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
+    # ^ REMOVED - this is tested implicitly via Django Test Client
 
     # Second added test
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  # HttpRequest object creation
-        response = home_page(request)  # request passed to home_page view
+        # request = HttpRequest()  # HttpRequest object creation
+        # response = home_page(request)  # request passed to home_page view
+        # ^ REMOVED (replaced with Django Test Client tool self.client.get('/')
+
+        response = self.client.get('/')
+
         html = response.content.decode('utf8')  # extract content of response and decode and convert to HTML
-        self.assertTrue(html.startswith('<html>'), html)  # check page starts with html tag
-        self.assertIn('<title>To-Do lists</title>', html)  # check title is 'To-Do list'
-        self.assertTrue(html.endswith('</html>'))  # check page ends with closing html tag
+
+        # self.assertTrue(html.startswith('<!DOCTYPE html>'), html)  # check page starts with html tag
+        # self.assertIn('<title>To-Do lists</title>', html)  # check title is 'To-Do list'
+        # self.assertTrue(html.strip().endswith('</html>'))  # check page ends with closing html tag
+
+        # expected_html = render_to_string('home.html')
+        # self.assertEqual(html, expected_html)
+        # ^ REMOVED, replaced with assertTemplatedUsed method
+        self.assertTemplateUsed(response, 'home.html')
+"""
+
+
+class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
