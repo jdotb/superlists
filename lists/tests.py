5f07f4867ea824cd794f0c2f9f6119e14e315b28
jdotb <jdotbdotbold@gmail.com>
2021-09-30 15:03:29 -0400

    New URL, view and template to display list. Home page now just wants input and lists diplayed on their own page

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import Item
+
 # from django.http import HttpRequest
 # ^ REMOVED since using self.client.get()
 
@@ -45,15 +46,14 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
 
-        self.assertEqual(Item.objects.count(), 1)   # check one item saved to db
-        new_item = Item.objects.first()             # objects.first() == objects.all()[0]
+        self.assertEqual(Item.objects.count(), 1)  # check one item saved to db
+        new_item = Item.objects.first()  # objects.first() == objects.all()[0]
         self.assertEqual(new_item.text, 'A new list item')
-
 
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
         # self.assertIn('A new list item', response.content.decode())
         # self.assertTemplateUsed(response, 'home.html')
@@ -65,14 +65,15 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_the_list_items(self):
-            Item.objects.create(text='itemey 1')
-            Item.objects.create(text='itemey 2')
+    # def test_displays_all_the_list_items(self):
+    #     Item.objects.create(text='itemey 1')
+    #     Item.objects.create(text='itemey 2')
+    #
+    #     response = self.client.get('/')
+    #
+    #     self.assertIn('itemey 1', response.content.decode())
+    #     self.assertIn('itemey 2', response.content.decode())
 
-            response = self.client.get('/')
-
-            self.assertIn('itemey 1', response.content.decode())
-            self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
@@ -92,3 +93,19 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-view-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
