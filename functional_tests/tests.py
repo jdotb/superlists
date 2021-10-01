8b90349e918574c805ce50b15e82933304c015ed
jdotb <jdotbdotbold@gmail.com>
2021-10-01 16:25:12 -0400

    pre-commit prior to deploy setup

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 import unittest
-
+STAGING_SERVER = "superlists.jdotbdotb.com"
 MAX_WAIT = 10
 
 
@@ -13,6 +14,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
