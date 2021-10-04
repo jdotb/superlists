66f0e6a413faa04f6afc9910fe2a4b2d251fb072
Jdotb <jdotbdotbold@gmail.com>
2021-10-04 12:33:45 -0400

    fabric must be run in python2.7 - script must use python2.7, removed f'strings

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -3,10 +3,12 @@ from fabric.api import env, local, run
 import random
 
 REPO_URL = 'https://github.com/jdotb/superlists.git'
+user = env.user
+host = env.host
 
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
+    site_folder = '/home/'+env.user+'/sites/'+ env.host
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
@@ -18,49 +20,47 @@ def deploy():
 
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('db', 'static', 'virtualenv', 'source'):
-        run(f'mkdir -p {site_folder}/{subfolder}')
+        run('mkdir -p '+site_folder+'/'+subfolder)
 
 
 def _get_latest_source(source_folder):
-    if exists(source_folder + '/.git'):
-        run(f'cd {source_folder} && git fetch')
+    if exists(source_folder+'/.git'):
+        run('cd '+source_folder+' && git fetch')
     else:
-        run(f'git clone {REPO_URL} {source_folder}')
+        run(f'git clone '+REPO_URL+' {source_folder}')
     current_commit = local("git log -n 1 --format=%H", capture=True)
-    run(f'cd {source_folder} && git reset --hard {current_commit}')
+    run('cd '+source_folder+' && git reset --hard '+current_commit)
 
 
 def _update_settings(source_folder, site_name):
-    settings_path = source_folder + '/superlists/settings.py'
+    settings_path = source_folder+'/superlists/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
-        f'ALLOWED_HOSTS = ["{site_name}"]'
+        'ALLOWED_HOSTS = ["'+site_name+'"]'
         )
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, f'SECRET_KEY = "{key}"')
+        append(secret_key_file, 'SECRET_KEY = "'+key+'"')
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 
 def _update_virtualenv(source_folder):
-    virtualenv_folder = source_folder + '/../virtualenv'
+    virtualenv_folder = source_folder+'/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
-        run(f'python3.9 -m venv {virtualenv_folder}')
-    run(f'{virtualenv_folder}/bin/pip install -r {source_folder}/requirements.txt')
+        run('python3.9 -m venv '+virtualenv_folder)
+    run(virtualenv_folder+'/bin/pip install -r '+source_folder+'/requirements.txt')
 
 
 def _update_static_files(source_folder):
     run(
-        f'cd {source_folder}'
-        ' && ../virtualenv/bin/python manage.py collectstatic --noinput'
+        'cd '+source_folder+' && ../virtualenv/bin/python manage.py collectstatic --noinput'
     )
 
 
 def _update_database(source_folder):
     run(
-        f'cd {source_folder}'
-        ' && ../virtualenv/bin/python manage.py migrate --noinput'
+        'cd '+source_folder+' && ../virtualenv/bin/python manage.py migrate --noinput'
     )
