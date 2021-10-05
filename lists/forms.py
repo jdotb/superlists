e014fcb11c503aece7d27c1c242b0b1629382e65
Jdotb <jdotbdotbold@gmail.com>
2021-10-05 14:08:18 -0400

    form added for list items

---
+++ lists/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from lists.models import Item
+
+EMPTY_ITEM_ERROR = "Empty list items aren't allowed"
+
+
+class ItemForm(forms.models.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+            }),
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
