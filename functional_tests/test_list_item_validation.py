3cfe2618e7feeb612787dfbb367b9295c798e2de
Jdotb <jdotbdotbold@gmail.com>
2021-10-05 14:47:22 -0400

    rename all item input ids and names...still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,7 +9,7 @@ class ItemValidationTest(FunctionalTest):
         # # James goes to the home page and accidentally tries to submit an empty list item
         ## - he hits ENTER on the empty input box
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         ## The homepage refreshes and shows an error message saying list items can't be blank
         self.wait_for(lambda: self.assertEqual(
@@ -17,12 +17,12 @@ class ItemValidationTest(FunctionalTest):
             "Empty list items aren't allowed"
         ))
         ## He tries again with some text, and it now works
-        self.browser.find_element(By.ID, 'id_new_item').send_keys('Buy cheese')
-        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Buy cheese')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy cheese')
 
         ## Jokingly, he submits another blank list
-        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         ## He receives a similar warning on the list page
         self.wait_for(lambda: self.assertEqual(
@@ -31,7 +31,7 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         ## This can be corrected by filling in some text
-        self.browser.find_element(By.ID, 'id_new_item').send_keys('Make lunch for tomorrow')
-        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Make lunch for tomorrow')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy cheese')
         self.wait_for_row_in_list_table('2: Make lunch for tomorrow')
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -15,7 +15,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         ## He is invited to enter a to-do item ##
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
 
@@ -33,7 +33,7 @@ class NewVisitorTest(FunctionalTest):
         # self.assertIn('1: Buy new drone', [row.text for row in rows])
 
         ## James notices a new text box awaits his input, so he enters "Find a cool place to fly new drone"
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Find a cool place to fly new drone')
         inputbox.send_keys(Keys.ENTER)
 
@@ -46,7 +46,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         ## James starts a new to-do lists
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy new drone')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy new drone')
@@ -68,7 +68,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertNotIn('fly new drone', page_text)
 
         # Candice starts a new list by entering a new item. She is less interesting than James...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy cheese')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy cheese')
