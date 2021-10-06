b2ac0555c0f2c80af77a42a37535a3d5715e4fb3
jdotb <jdotbdotbold@gmail.com>
2021-10-06 19:44:25 +0000

    made changes to settings and base.html in remote host

--- superlists/settings.py
+++ superlists/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.FileSystemFinder',
+'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../../static/lists'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static/lists'))
