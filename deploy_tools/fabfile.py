3b6e7f0d192766b0d93136e9872b88ef91e8cccb
Jdotb <jdotbdotbold@gmail.com>
2021-10-10 17:26:24 -0400

    fixed fabfile to be python2.7 compatiable, provision_notes updated for services

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -20,16 +20,16 @@ def deploy():
 
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('db', 'static', 'virtualenv', 'source'):
-        run('mkdir -p ' + site_folder + '/' + subfolder)
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
 
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
-        run('cd ' + source_folder + ' && git fetch')
+        run('cd %s && git fetch' % source_folder)
     else:
-        run('git clone ' + REPO_URL + ' '+source_folder)
-    current_commit = run('git log -n 1 --format=%H')
-    run('cd ' + source_folder + ' && git reset --hard ' + current_commit)
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
 
 def _update_settings(source_folder, site_name):
@@ -37,30 +37,29 @@ def _update_settings(source_folder, site_name):
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
-        'ALLOWED_HOSTS = ["' + site_name + '"]'
-        )
+        'ALLOWED_HOSTS = ["%s"]' % site_name)
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, 'SECRET_KEY = "' + key + '"')
+        append(secret_key_file, "SECRET_KEY = '%s'" % key)
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
-        run('python3.9 -m venv ' + virtualenv_folder)
-    run(virtualenv_folder + '/bin/pip install -r ' + source_folder + '/requirements.txt')
+        run('virtualenv --python=python3 %s' % virtualenv_folder)
+    run('%s/bin/pip install -r %s/requirements.txt' % (
+        virtualenv_folder, source_folder
+    ))
 
 
 def _update_static_files(source_folder):
-    run(
-        'cd ' + source_folder + ' && ../virtualenv/bin/python manage.py collectstatic --noinput'
-    )
+    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % (
+        source_folder))
 
 
 def _update_database(source_folder):
-    run(
-        'cd ' + source_folder + ' && ../virtualenv/bin/python manage.py migrate --noinput'
-    )
+    run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % (
+        source_folder))
--- deploy_tools/provisioning_notes.md
+++ deploy_tools/provisioning_notes.md
@@ -17,9 +17,30 @@ Provisioning a new site
 * replace SITENAME with, e.g., staging.my-domain.com
 
 ## Systemd service
-* see gunicorn-systemd.template.service
-* replace SITENAME with, e.g., stage.my-domain.com
+1. Add a new service with this command:
+    > **sudo nano /etc/systemd/system/gunicorn-SITENAME.service**
 
+    a. add the config file using *gunicorn-systemd.template*
+
+
+2. Load the new service:
+    > **sudo systemctl daemon-reload**
+
+3. Tell Systemd to always load service on boot:
+    > **sudo systemctl enable gunicorn-SITENAME**
+
+4. Start the service:
+    > **sudo systemctl start gunicorn-SITENAME**
+
+    i. see *gunicorn-systemd.template.service*
+
+    ii. replace SITENAME with the site's name, e.g., stage.my-domain.com
+
+
+* NOTE: for debugging...
+  > **sudo journalctl -u gunicorn-SITENAME**
+* ask Systemd to check validity of service config:
+  > **systemd-analyze verify /path/to/my.service**
 ## Folder structure:
 Assume we have a user account at /home/username:
 
