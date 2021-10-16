1c5ddf428ff988e85e47871c9350582e92470db4
Jdotb <jdotbdotbold@gmail.com>
2021-10-15 20:17:46 -0400

    updated

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -25,6 +25,19 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def wait(fn):
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn
+
+    @wait
     def wait_for(self, fn):
         start_time = time.time()
         while True:
@@ -34,7 +47,7 @@ class FunctionalTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -51,13 +64,14 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         return self.browser.find_element(By.ID, 'id_text')
 
+    @wait
     def wait_to_be_logged_in(self,email):
         self.wait_for(
             lambda:self.browser.find_element(By.LINK_TEXT, 'Log out')
         )
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertIn(email, navbar.text)
-
+    @wait
     def wait_to_be_logged_out(self,email):
         self.wait_for(
             lambda: self.browser.find_element(By.NAME, 'email')
